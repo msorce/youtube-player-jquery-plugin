904164ceaffc719eecf9b4efad2ad6cc6e9f8887
msorce <msorce@users.noreply.github.com>
2015-11-13 16:41:05 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+$(function() {
+  (function($) {
+    'use strict';
+    $.fn.ytplugin = function(options) { //plugin start
+      return this.each(function() {
+        var $this = $(this);
+        var vidId = $this.attr('data-vid');
+        var settings = $.extend({
+          id: vidId
+        }, options);
+        // var elName = $this.attr('id');
+        var elName = $this.attr('id');
+        var player = null;
+        var eventYouTubePlayerAPIReady = null;
+        function onPlayerReady(event) {
+          console.log(event.target.getPlayerState());
+        }
+        if (typeof(window.onYouTubePlayerAPIReady) == 'function') {
+          eventYouTubePlayerAPIReady = window.onYouTubePlayerAPIReady;
+        }
+        window.onYouTubePlayerAPIReady = function(event) {
+          player = new YT.Player(elName, {
+            videoId: settings.id,
+            events: {
+              'onReady': onPlayerReady
+            }
+          });
+          if (eventYouTubePlayerAPIReady) {
+            eventYouTubePlayerAPIReady(event);
+          }
+        };
+      });
+    }; //plugin end
+  }(jQuery));
+});
